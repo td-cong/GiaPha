8c8600d622eb7203383d8a9cca1d15f2f6ced180
c td <dangcong83@gmail.com>
2023-11-03 13:58:50 +0700

    Cong.03112023.1400

---
+++ Scripts/thugon.js
@@ -0,0 +1,30 @@
+﻿(function ($) {
+    $(document).ready(function () {
+        $(window).on('load', function () {
+            if ($('.single-product div#tab-description').length > 0) {
+                let wrap = $('.single-product div#tab-description');
+                let current_height = wrap.height();
+                let your_height = 800;
+                if (current_height > your_height) {
+                    wrap.addClass('fix_height');
+                    wrap.append(function () {
+                        return '<div class="giuseart_readmore_flatsome giuseart_readmore_flatsome_more"><a title="Xem thêm" href="javascript:void(0);">Xem thêm</a></div>';
+                    });
+                    wrap.append(function () {
+                        return '<div class="giuseart_readmore_flatsome giuseart_readmore_flatsome_less" style="display: none;"><a title="Xem thêm" href="javascript:void(0);">Thu gọn</a></div>';
+                    });
+                    $('body').on('click', '.giuseart_readmore_flatsome_more', function () {
+                        wrap.removeClass('fix_height');
+                        $('body .giuseart_readmore_flatsome_more').hide();
+                        $('body .giuseart_readmore_flatsome_less').show();
+                    });
+                    $('body').on('click', '.giuseart_readmore_flatsome_less', function () {
+                        wrap.addClass('fix_height');
+                        $('body .giuseart_readmore_flatsome_less').hide();
+                        $('body .giuseart_readmore_flatsome_more').show();
+                    });
+                }
+            }
+        });
+    });
+})(jQuery);
